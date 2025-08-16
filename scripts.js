7a03d25bc26a42abff71c8362c2703350e2e4844
BogdanROSs <155568150+BogdanROSs@users.noreply.github.com>
2025-08-16 04:00:18 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const stars = document.getElementById('stars');
+for (let i = 0; i < 200; i++) {
+    const star = document.createElement('div');
+    star.className = 'star';
+    star.style.width = `${Math.random() * 3}px`;
+    star.style.height = star.style.width;
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.animationDuration = `${Math.random() * 2 + 1}s`;
+    stars.appendChild(star);
+}
+
+const starRain = document.getElementById('star-rain');
+setInterval(() => {
+    const star = document.createElement('div');
+    star.className = 'star';
+    star.style.width = `${Math.random() * 3}px`;
+    star.style.height = star.style.width;
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.animationDuration = `${Math.random() * 2 + 1}s`;
+    starRain.appendChild(star);
+
+    setTimeout(() => {
+        star.remove();
+    }, 5000);
+}, 5000);
